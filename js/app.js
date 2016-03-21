fdb3fd5df51366789518c2aa61875898209d32dd
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-03-20 17:50:54 -0700

    First draft: tic-tac-toe

--- js/app.js
+++ js/app.js
@@ -3,4 +3,29 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+//save result in a variable
+var $boxes = $('.box');
+
+
+  // player "X" always goes first, and player "O" always goes second
+  var move = "X";
+
+function nextMove() {
+  if (move==="X") {
+    move = "O";
+  } else {
+    turn = "X";
+  }
+
+  }
 });
+
+// check for winner
+
+function allThreeBoxes($boxOne, $boxTwo, $boxThree) {
+  var boxOneOwner = $boxOne.text();
+  var boxTwoOwner = $boxTwo.text();
+  var boxThreeOwner = $boxThree.text();
+
+
+}
